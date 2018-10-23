fd59587f7704709c4a97a3161764a3ea5ef48a1b
Kristoffer Katra <doedmannen@aetmej.se>
2018-10-23 11:24:19 +0200

    Added newEntry() so that we can add a value to a specific gridposition

--- solver.js
+++ solver.js
@@ -10,16 +10,31 @@
 9 8 7 6 5 4 3 2 1
 
 */
-
+// Grid for the sudoku
 let gridCells = [];
 
 createGrid();
 
+newEntry(2, 3, 9);
+
+
+// Creates a 2D array with new cells
 function createGrid() {
   for(let r = 0; r < 9; r++){
     gridCells[r] = [];
     for(let c = 0; c < 9; c++){
       gridCells[r][c] = new Cell(r, c);
     }
+  }
+}
+
+
+// Adds a value to a certain gridposition and changes the boolean soft to false
+function newEntry(r, c, v) {
+  if(v < 10 && v > 0 && r > 0 && r < 10 && c > 0 && c < 10){
+    r--;
+    c--;
+    gridCells[r][c].value = v;
+    gridCells[r][c].soft = false;
   }
 }
