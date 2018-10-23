3b82517f1d9001d95c597683e30900288ae5d52e
Kristoffer Katra <doedmannen@aetmej.se>
2018-10-23 20:32:43 +0200

    Added method for checking if cells value is valid

--- cell.js
+++ cell.js
@@ -7,4 +7,32 @@ class Cell {
     this.value = 0;
   }
 
+  findVal(){
+    if(!this.soft)
+      return true;
+
+      // Testar rad och kolumn
+      for(let i = 0; i < 9; i++){
+        if(gridCells[this.r][i].value == v || gridCells[i][this.c].value == v){
+          return false;
+        }
+      }
+
+      // Testar om värdet finns i rutan
+      let r = int(this.r/3)*3;
+      let c = int(this.c/3)*3;
+
+      for(let i = 0; i < 3; i++){
+        for(let j = 0; j < 3; j++){
+          if(gridCells[(r+i)][(c+j)].value == v){
+            return false;
+          }
+        }
+      }
+
+      this.value = v;
+      return true;
+
+
+  }
 }
